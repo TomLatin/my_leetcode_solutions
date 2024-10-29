c98fdc23fc975e42181c86457d05e9dd1ed82555
TomLatin <57855070+TomLatin@users.noreply.github.com>
2024-10-29 13:34:18 +0200

    done

---
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,8 @@
+def maxProfit(prices: list[int]) -> int:
+    min_price = float('inf')
+    max_profit = 0
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        max_profit = max(max_profit, price - min_price)
+    return max_profit
